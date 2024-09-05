ac8312acec27b3ec578b0b7fb64f6f325149ffda
CybroSCDev <>
2024-09-05 05:18:21 +0300

    Moved Izumi

--- src/interfaces/izumi/IBase.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IBase {
-    function WETH9() external returns (address);
-    function factory() external returns (address);
-    function refundETH() external payable;
-}
